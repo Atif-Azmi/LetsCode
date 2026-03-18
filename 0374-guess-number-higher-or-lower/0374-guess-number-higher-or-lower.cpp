381e01e72160de57babd5af9e2bba9ffcb40e30e
Atif-Azmi <atifazmi071005@gmail.com>
2026-03-18 15:15:16 +0530

    Time: 2 ms (57.2%), Space: 8 MB (5.89%) - LeetHub

---
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -0,0 +1,19 @@
+// forward declaration of guess API
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int l = 1, r = n;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            int res = guess(mid);
+            if (res == 0) return mid;
+            else if (res == -1) r = mid - 1;
+            else l = mid + 1;
+        }
+        return -1;
+    }
+};
